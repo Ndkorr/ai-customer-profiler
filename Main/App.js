cdad49a4f08a5f1e4f93057b799dcc0289195259
mvthewsa <mathew_astorga@trendmicro.com>
2025-09-05 05:37:05 +0800

    #1 Created UI using js

---
+++ Main/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import MainDashboard from "./main";
+import "./main.css"; // Make sure your styles are applied
+
+function App() {
+  return <MainDashboard />;
+}
+
+export default App;
--- Main/main.js
+++ Main/main.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./MainDashboard.css";
 
 export default function MainDashboard() {
   return (
